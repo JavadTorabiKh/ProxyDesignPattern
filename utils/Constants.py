db25d81b236ff5d7204e33ebc9660c0d39bcac73
javadTorabiKh <j.2528840@gmail.com>
2025-05-14 17:00:21 +0330

    fix

--- utils/Constants.py
+++ utils/Constants.py
@@ -5,6 +5,7 @@ password_pattern = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$"
 
 EmailValid = {"messageEmailValid": "Email is Valid"}
 EmailNOTValid = {"messageEmailNOTValid": "This is a duplicate email"}
+
 UserValid = {"messageUserValid": "Username is Valid"}
 UserNOTValid = {"messageUserNOTValid": "This is a duplicate Username"}
 success = {"messageSuccess": "Your registeration was Success"}
@@ -13,4 +14,5 @@ checkUsernameMessage = {"checkUsernameMessage": "Your Username is invalid"}
 checkFullnameMessage = {"checkFullnameMessage": "Your Email is invalid"}
 checkEmailMessage = {"checkEmailMessage": "Your Email is invalid"}
 checkPasswordMessage = {"checkPasswordMessage": "Your Password is invalid"}
-checkConfirmpassMessage = {"checkConfirmpassMessage": "Confirm password does not match"}
+checkConfirmpassMessage = {
+    "checkConfirmpassMessage": "Confirm password does not match"}
